968c9f9097b5855428d1f3b56240be5463d7d7f0
KwonNam Son <kwon37xi@gmail.com>
2022-01-10 17:38:30 +0900

    render 기본 틀 구현

--- src/react.js
+++ src/react.js
@@ -1,5 +1,22 @@
-export function render(component, container) {
-  console.log(component);
+function renderRealDOM(vdom) {
+  // 자식이 없는 경우
+  if (vdom === undefined) return;
+
+  // 마지막 요소는 text node 라서 출력할 문자열임.
+  if (typeof vdom === "string") {
+    return document.createTextNode(vdom);
+  }
+
+  const $el = document.createElement(vdom.tagName);
+
+  vdom.children.map(renderRealDOM).forEach((node) => {
+    $el.appendChild(node);
+  });
+  return $el;
+}
+
+export function render(vdom, container) {
+  container.appendChild(renderRealDOM(vdom));
 }
 
 export function createElement(tagName, props, ...children) {
